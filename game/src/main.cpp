70cb3f9519ee1592decd6a25ce32d42eb4446b00
PrismFae <sky0522.c@icloud.com>
2025-09-21 20:33:01 -0400

    Lab 1

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -12,34 +12,48 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 const unsigned int TARGET_FPS = 50; //frames/second
 float dt = 1.0f / TARGET_FPS; //seconds/frame
 float time = 0;
-float x = 500;
-float y = 500;
-float frequency = 1;
-float amplitude = 100;
+
+Vector2 launchPosition = { 200, 200 };
+//Vector2 launchVelocity = { 200, 0 };
+float launchAngle = 300.0f;
+float launchSpeed = 200.0f;
+Vector2 launchVelocity = { 0, 0 };
 
 //Changes world state
 void update()
 {
 	dt = 1.0f / TARGET_FPS;
 	time += dt;
-
-	x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
-	y = y + (cos(time * frequency)) * frequency * amplitude * dt;
 }
 
 //Display world state
 void draw()
 {
 	BeginDrawing();
-	ClearBackground(BLACK);
-	DrawText("Joss Moo-Young 123456789", 10, float(GetScreenHeight() - 30), 20, LIGHTGRAY);
+	ClearBackground(WHITE);
 
+	// Slider variables
+	float rectangleWidth = 400;
+	GuiSliderBar(Rectangle{ InitialWidth - rectangleWidth - 60, 15, rectangleWidth, 20 }, "Launch Angle", 
+		TextFormat("%.2f", launchAngle), &launchAngle, 0, 360.0f);
+	GuiSliderBar(Rectangle{ InitialWidth - rectangleWidth - 60, 45, rectangleWidth, 20 }, "Launch Speed", 
+		TextFormat("%.2f", launchSpeed), &launchSpeed, 50, 500);
+	GuiSliderBar(Rectangle{ InitialWidth - rectangleWidth - 60, 75, rectangleWidth, 20 }, "Launch X", 
+		TextFormat("%.2f", launchPosition.x), &launchPosition.x, 0, GetScreenWidth());
+	GuiSliderBar(Rectangle{ InitialWidth - rectangleWidth - 60, 105, rectangleWidth, 20 }, "Launch Y", 
+		TextFormat("%.2f", launchPosition.y), &launchPosition.y, 0, GetScreenHeight());
 
-	GuiSliderBar(Rectangle{ 10, 15, 1000, 20 }, "", TextFormat("%.2f", time), &time, 0, 240);
-	DrawText(TextFormat("T: %6.2f", time), GetScreenWidth() - 140, 10, 30, LIGHTGRAY);
+	// Text for slider variables
+	DrawText(TextFormat("Angle: %.2f", launchAngle), 10, 15, 20, BLACK);
+	DrawText(TextFormat("Speed: %.2f", launchSpeed), 10, 45, 20, BLACK);
+	DrawText(TextFormat("Launch Position: (%.2f, %.2f)", launchPosition.x, launchPosition.y), 10, 75, 20, BLACK);
 
-	DrawCircle(x, y, 70, RED);
-	DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN);
+	// Circle representing the launch position
+	DrawCircleV(launchPosition, 10, ORANGE);
+
+	// Line representing the launch angle and speed
+	Vector2 velocityVector = Vector2Rotate(Vector2UnitX, DEG2RAD * launchAngle) * launchSpeed;
+	DrawLineV(launchPosition, launchPosition + velocityVector, RED);
 
 	EndDrawing();
 
@@ -47,7 +61,7 @@ void draw()
 
 int main()
 {
-	InitWindow(InitialWidth, InitialHeight, "GAME2005 Joss Moo-Young 123456789");
+	InitWindow(InitialWidth, InitialHeight, "Angry Birds");
 	SetTargetFPS(TARGET_FPS);
 
 	while (!WindowShouldClose()) // Loops TARGET_FPS times per second
